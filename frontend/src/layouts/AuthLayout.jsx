c8bca776d77b39feb307129f6be55d1c28b7dc35
BrAcKeTzone <jpskiemaniwang@gmail.com>
2025-12-20 21:03:09 +0800

    Refactor code structure for improved readability and maintainability

--- frontend/src/layouts/AuthLayout.jsx
+++ frontend/src/layouts/AuthLayout.jsx
@@ -3,7 +3,7 @@ import Navbar from "../components/layout/Navbar";
 
 const AuthLayout = ({ children }) => {
   return (
-    <div className="flex flex-col min-h-screen bg-white">
+    <div className="flex flex-col min-h-screen">
       <Navbar />
       <main className="flex-1 text-gray-900">{children}</main>
     </div>
